7977eb439d43935307cd40541d3b514aacca655b
Shubham Kumar Jha <kumarshubhamjha2157@gmail.com>
2020-07-20 18:51:47 +0530

    SignUp

--- app/src/main/java/com/shubham/weather/ui/SignUp.java
+++ app/src/main/java/com/shubham/weather/ui/SignUp.java
@@ -1,50 +1,120 @@
 package com.shubham.weather.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
-
 import com.example.weatherchecker.R;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.shubham.weather.db.UserHelperClass;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignUp extends AppCompatActivity {
+    public static final String TAG = "TAG";
+    TextInputLayout mName,  mEmail, mPhone, mPassword;
+    Button mRegisterBtn, mLoginBtn;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
-    TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
-    Button regBtn, regToLoginBtn;
-    FirebaseDatabase rootNode;
-    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up);
 
-        regName = findViewById(R.id.reg_name);
-        regUsername = findViewById(R.id.reg_username);
-        regEmail = findViewById(R.id.reg_email);
-        regPhoneNo = findViewById(R.id.reg_phoneNo);
-        regPassword = findViewById(R.id.reg_password);
-        regBtn = findViewById(R.id.reg_btn);
-        regToLoginBtn = findViewById(R.id.reg_login_btn);
+        mName = findViewById(R.id.reg_name);
+        //regUsername = findViewById(R.id.reg_username);
+        mEmail = findViewById(R.id.reg_email);
+        mPhone = findViewById(R.id.reg_phoneNo);
+        mPassword = findViewById(R.id.reg_password);
+        mRegisterBtn = findViewById(R.id.reg_btn);
+        mLoginBtn = findViewById(R.id.reg_login_btn);
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        //progressBar = findViewById(R.id.progressBar);
 
-        regBtn.setOnClickListener(view -> {
-            rootNode = FirebaseDatabase.getInstance();
-            reference = rootNode.getReference("users");
-            String name = regName.getEditText().getText().toString();
-            String username = regUsername.getEditText().getText().toString();
-            String email = regEmail.getEditText().getText().toString();
-            String phoneNo = regPhoneNo.getEditText().getText().toString();
-            String password = regPassword.getEditText().getText().toString();
-            UserHelperClass helperClass = new UserHelperClass(name, username, email, phoneNo, password);
-            reference.child(phoneNo).setValue(helperClass);
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+
+        mRegisterBtn.setOnClickListener(v -> {
+            final String email = mEmail.getEditText().getText().toString().trim();
+            String password = mPassword.getEditText().getText().toString().trim();
+            final String fullName = mName.getEditText().getText().toString();
+            final String phone    = mPhone.getEditText().getText().toString();
+
+            if(TextUtils.isEmpty(email)){
+                mEmail.setError("Email is Required.");
+                return;
+            }
+
+            if(TextUtils.isEmpty(password)){
+                mPassword.setError("Password is Required.");
+                return;
+            }
+
+            if(password.length() < 6){
+                mPassword.setError("Password Must be >= 6 Characters");
+                return;
+            }
+
+            //progressBar.setVisibility(View.VISIBLE);
+
+            // register the user in firebase
+
+            fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(task -> {
+                if(task.isSuccessful()){
+
+                    // send verification link
+
+                    FirebaseUser fuser = fAuth.getCurrentUser();
+                    fuser.sendEmailVerification().addOnSuccessListener(aVoid -> Toast.makeText(SignUp.this, "Verification Email Has been Sent.", Toast.LENGTH_SHORT).show()).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "onFailure: Email not sent " + e.getMessage());
+                        }
+                    });
+
+                    Toast.makeText(SignUp.this, "User Created.", Toast.LENGTH_SHORT).show();
+                    userID = fAuth.getCurrentUser().getUid();
+                    DocumentReference documentReference = fStore.collection("users").document(userID);
+                    Map<String,Object> user = new HashMap<>();
+                    user.put("fName",fullName);
+                    user.put("email",email);
+                    user.put("phone",phone);
+                    documentReference.set(user).addOnSuccessListener(aVoid -> Log.d(TAG, "onSuccess: user Profile is created for "+ userID)).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.d(TAG, "onFailure: " + e.toString());
+                        }
+                    });
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+                }else {
+                    Toast.makeText(SignUp.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                  //  progressBar.setVisibility(View.GONE);
+                }
+            });
         });
+
+
+
+        mLoginBtn.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),Login.class)));
 
     }
 }
